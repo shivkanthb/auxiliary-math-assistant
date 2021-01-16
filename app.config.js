7433a9b94588890a0b3f35c805ce9a3505db370e
shiv <shivkanthb@gmail.com>
2021-01-16 14:45:08 -0800

    update config for new build

--- app.config.js
+++ app.config.js
@@ -1,8 +1,33 @@
 import "dotenv/config";
 
 export default {
-  name: "Euler",
-  version: "1.0.0",
+  name: "Euler Assist",
+  slug: "euler",
+  version: "1.0.2",
+  orientation: "portrait",
+  icon: "./assets/euler.png",
+  splash: {
+    image: "./assets/splash-euler.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.shivkanth.euler",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#FFFFFF",
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
   extra: {
     MATHPIX_API_ENDPOINT: process.env.MATHPIX_API_ENDPOINT,
     MATHPIX_APP_ID: process.env.MATHPIX_APP_ID,
