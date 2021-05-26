6de2674905cff17d504f5dbb5c0ca8c065e176ef
shiv <shivkanthb@gmail.com>
2021-05-26 14:35:31 -0700

    bumping to 1.0.4

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 export default {
   name: "Euler Assist",
   slug: "euler",
-  version: "1.0.3",
+  version: "1.0.4",
   orientation: "portrait",
   icon: "./assets/euler.png",
   splash: {
